71f95e0a4c318f49560ad14f80b12d1f8013c244
KasraMg <kasragithub7@gmail.com>
2024-11-24 23:37:36 +0330

    fix home helps

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ const index = () => {
   return (
     <div>
       <Link className='mx-auto block mt-52 w-max text-4xl' to={'/adminPanel/users'}>پنل ادمین </Link>
+      <Link className='mx-auto block mt-10 w-max text-4xl' to={'/login'}>ورود   </Link> 
     </div>
   )
 }
